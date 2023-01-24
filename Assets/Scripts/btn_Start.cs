2a016b257f16ca2584e52f8b1d64e7628b5de178
Snowkai <34677392+Snowkai@users.noreply.github.com>
2023-01-24 12:59:52 +0300

    Button Start finished.

--- Assets/Scripts/btn_Start.cs
+++ Assets/Scripts/btn_Start.cs
@@ -12,7 +12,7 @@ public class btn_Start : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        Instantiate(ball, position, rotation, parent);
+        ball = Instantiate(ball, position, rotation, parent);
     }
 
     // Update is called once per frame
@@ -26,10 +26,11 @@ public class btn_Start : MonoBehaviour
         Rigidbody2D bd = ball.GetComponent<Rigidbody2D>();
         Vector2 dp = directPush.GetComponent<Transform>().position;
         Vector2 bp = ball.GetComponent<Transform>().position;
-        Vector2 result = dp - bp;
+        Vector2 result = dp -bp;
         Debug.Log(dp);
         Debug.Log(bp);
         Debug.Log(result);
-        bd.AddForce(result, ForceMode2D.Impulse);
+        bd.AddForce(result * 1.5f, ForceMode2D.Impulse);
+        gameObject.SetActive(false);
     }
 }
