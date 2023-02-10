6ad2bc27856696f635ecbe8fffa4633fa7649e81
Snowkai <34677392+Snowkai@users.noreply.github.com>
2023-02-10 18:19:26 +0300

    HighScore re-reworking

--- Assets/Scripts/Ball.cs
+++ Assets/Scripts/Ball.cs
@@ -1,19 +1,35 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SocialPlatforms.Impl;
 
 public class Ball : MonoBehaviour
 {
-
+    GameObject hg;
+    HighScore score;
+    public int mushroom;
+    public int platform;
     // Start is called before the first frame update
     void Start()
     {
-        
+        hg = GameObject.Find("HighScore");
+        score = hg.GetComponent<HighScore>();
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+    public void OnCollisionExit2D(Collision2D collision)
+    {
+        if(collision.gameObject.tag == "mushroom")
+        {
+            score.AddScore(mushroom);
+        }
+        if (collision.gameObject.tag == "platform")
+        {
+            score.AddScore(platform);
+        }
     }
 }
